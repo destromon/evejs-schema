4fbccb2975c3939a251f219e0852cf5465981990
destromon <jestadena@yahoo.com>
2014-06-24 11:53:34 +0800

    fixing type [] and field radio

--- schema.js
+++ schema.js
@@ -40,9 +40,8 @@ eden('sequence')
 //loop through schema and get properties
 .then(function(next) {
     var template = {};
-    var counter  = 0;
 
-    var _readKeys = function(content, schema, parent) {
+    var _readKeys = function(content, schema) {
         for(var key in content) {
             var field    = '';
             var type     = '';
@@ -52,68 +51,86 @@ eden('sequence')
             if(content[key].field !== undefined) {
                 type = content[key].field;
                 field = key;
-                console.log(key, 'type ->', type);
+                //console.log(key, 'type ->', type);
 
             //check if its select
             } else if(content[key].hasOwnProperty('enum')) {
                 field = key;
                 type  = 'select';
-                console.log(key, 'type ->', ' select')
+                //console.log(key, 'type ->', ' select')
             } else {
 
-                //get its type
+                //get field according to type
                 if(content[key].hasOwnProperty('type')) {
 
                     var field = content[key].type.toString().toLowerCase();
-                    console.log(field);
+                    //console.log(field);
                     //case if string
                     if (field.indexOf('string') !== -1 ) {
                         type = 'text';
-                        console.log(key, ' field is ', type);
+                        //console.log(key, ' field is ', type);
 
                     //case if boolean
                     } else if(field.indexOf('boolean') !== -1) {
                         type = 'radio';
-                        console.log(key, ' field is ', type);
+                        //console.log(key, ' field is ', type);
 
                     //case if date
                     } else if(field.indexOf('date') !== -1) {
                         type = 'datetime';
-                        console.log(key, ' field is ', type);
+                        //console.log(key, ' field is ', type);
                     } 
                     field = key;
                 } else {
-                    console.log (key, 'is array so its ewan pa');
+                    //console.log (key, 'is array so its ewan pa');
                 }
 
-                //check if its required!
-                if(content[key].hasOwnProperty('required')) {
-                    required = '<span>*</span>';
-                }
+                
             }
-                if(key === 'created' || key === 'updated') {
-                    break;
+            
+            if(key === 'created' || key === 'updated') {
+                break;
+            }
+
+            //check if its required!
+            if(content[key].hasOwnProperty('required')) {
+                required = ' <span>*</span>';
+            } else if(content[key].hasOwnProperty('data')) {
+                if (content[key].data.hasOwnProperty('required')){
+                    required = ' <span>*</span>';
                 }
-                //add to template
-                template += '{{{block '+ '\'field/'+type +'\' ' +field+' ../'+ schema+'.'+field +'}}}';
+                //_readKeys(content[key], schema);
+            }
+
+            var title = field.charAt(0).toUpperCase() + field.substring(1,field.length);
+
+             template += '{{#block ' +'\'form/fieldset\' '+  title + required + ' errors.'+field+'.message}}' +
+                    '{{{block '+ '\'field/'+type +'\' ' +field+' ../'+ schema+'.'+field +'}}}' + '{{/block}}';
         }
     }
 
     for(var schema in data)  {
+        //require schema
+        template = {};
         var content = require('./schema/' + schema);
-        console.log('reading schema!');
+
+        //get file name        
         var file = schema.toString().substring(0,schema.toString().length-3);
-        _readKeys(content, file, file);
-        console.log('printing template!!')
-        console.log(template);
+
+        //read property
+        _readKeys(content, file);
         
+        //create template
+        console.log('Creating template for ', file);
+        
+        //remove object on template
+        console.log(template);
         fs.writeFile(paths.dev + '/' + paths.schema + '/'+schema, template, function(err) {
             if (err) {
                 console.log('failed to create template');
             } else {
-                console.log('template successfully created');
+                console.log(file, 'template has been created.');
             }
         });
-        return;
     }
 })
